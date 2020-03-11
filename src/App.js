2c109ce6304510b67c24cf5e3db5dee91ee33f5c
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-03-11 12:45:14 +0200

    Create ColorBox Component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import seedColors from './seedColors';
 function App() {
   return (
     <div className="App">
-      <Palette colorPanel={seedColors}/>
+      <Palette {...seedColors[0]}/>
+      <Palette {...seedColors[1]}/>
+      <Palette {...seedColors[2]}/>
+      <Palette {...seedColors[3]}/>
     </div>
   );
 }
--- src/Palette.js
+++ src/Palette.js
@@ -1,23 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import ColorBox from "./ColorBox";
+import './Palette.css';
 
 class Palette extends Component {
-    render() {
-        const colorPanel = this.props.colorPanel.map(colorObj => {
-            const colorArr = colorObj.colors.map((color, i) => {
-                console.log(color);
-               return  <span key={i} style={{backgroundColor: color.color, display: 'block', height: 20 + 'px'}}></span>
-            });
-            return colorArr;
-        })
-        return (
-            <div>
-                <h1>Palette</h1>
-                <div>
-                    {colorPanel}
-                </div>
-            </div>
-        );
-    }
+  render() {
+      const colorBox = this.props.colors.map((color, i) => {
+          console.log(color);
+          return <ColorBox background={color.color} name={color.name} key={i}/>
+      })
+    // const colorPanel = this.props.colors.map(colorObj => {
+    //   const colorArr = colorObj.colors.map((color, i) => {
+    //     console.log(color);
+    //     return (
+    //       <span
+    //         key={i}
+    //         style={{
+    //           backgroundColor: color.color,
+    //           display: "inline-block",
+    //           height: 80 + "px",
+    //           width: 80 + "px",
+    //           marginBottom: -4 + "px"
+    //         }}
+    //       ></span>
+    //     );
+    //   });
+    //   return colorArr;
+    // });
+    return (
+      <div className="Palette">
+        <div className="Palette-color">{colorBox}</div>
+      </div>
+    );
+  }
 }
 
 export default Palette;

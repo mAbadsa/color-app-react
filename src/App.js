3b94b5cf76af1fd9715bd0bbc069b866edc244d2
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-03-16 18:36:13 +0200

    Add Palette Route by Id Params

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from "react";
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
 class App extends React.Component {
+  findPalette(id) {
+    return seedColors.find(palette => {
+      return palette.id === id;
+    });
+  }
   render() {
     console.log(generatePalette(seedColors[2]));
     return (
-        <Switch>
-          <Route exact path='/'render={() => <h1>Palette List</h1>}/>
-          <Route exact path='/palette/:id' render={() => <Palette palette={generatePalette(seedColors[3])} /> }/>
-        </Switch>
+      <Switch>
+        <Route exact path="/" render={() => <h1>Palette List</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={routeProps => (
+            <Palette
+              palette={generatePalette(this.findPalette(routeProps.match.params.id))}
+            />
+          )}
+        />
+      </Switch>
       // <div className="App">
       // </div>
     );

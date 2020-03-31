cccf4c64691a59af513b38178a61873f492dc82a
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-04-01 02:25:26 +0300

    Style DraggableColorBox

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -1,24 +1,51 @@
 import React from "react";
 import { withStyles } from "@material-ui/styles";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 const styles = {
   root: {
-    // display: "flex",
     margin: "0 auto",
     // marginBottom: "-4px",
     width: "20%",
     height: "25%",
     position: "relative",
     display: "inline-block",
-    cursor: "pointer"
+    cursor: "pointer",
+    "&:hover svg": {
+      color: "white",
+      transform: "scale(1.3)",
+      // transition: "all 0.3s ease-in-out"
+    }
+  },
+  boxContent: {
+    display: "flex",
+    position: "absolute",
+    justifyContent: "space-between",
+    width: "100%",
+    bottom: 0,
+    left: 0,
+    padding: "10px",
+    color: "rgba(0, 0, 0, 0.5)",
+    letterSpacing: "1px",
+    textTransform: "uppercase",
+    fontSize: "12px"
+  },
+  deleteIcon: {
+    transition: "all 0.3s ease-in-out",
   }
 };
 
 function DraggableColorBox(props) {
   const { classes } = props;
+  const handleDelete = () => {};
   return (
     <div className={classes.root} style={{ backgroundColor: props.color }}>
-      {props.colorName}
+      <div className={classes.boxContent}>
+        <span>{props.colorName}</span>
+        <span>
+          <DeleteIcon className={classes.deleteIcon} onClick={handleDelete} />
+        </span>
+      </div>
     </div>
   );
 }

84a50e0559bf317a6ee3066ef7359be5f89633fd
mAbadsa <moh22389@gmail.com>
2023-03-01 15:52:57 +0200

    deploy to S3 bucket by custom javascript actions

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,15 @@ const github = require("@actions/github");
 const exec = require("@actions/exec");
 
 function run() {
+  const bucketName = core.getInput("bucket", { required: true });
+  const bucketRegion = core.getInput("bucket-region", { required: true });
+  const buildDistFolder = core.getInput("dist-folder", { required: true });
+
+  // Upload files to AWS S3 Bucket
+  const s3Uri = `s3://${bucketName}`;
+  exec.exec(
+    `aws s3 sync ${buildDistFolder} ${s3Uri} --region ${bucketRegion} --delete`
+  );
   core.notice("Hello from my custom JavaScript action.");
 }
 
